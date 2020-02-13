c5733d8833c63bf717ef7689f534c5d7b10fc894
Gilberto Diaz <diaz.gilberto@gmail.com>
2020-02-13 16:38:34 -0600

    Initial application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  password = ''
+  passwordLength = 0
+  includeLetters = false
+  includeNumbers = false
+  includeSymbols = false
+
+  disabledBth() {
+    return !(this.passwordLength !== 0 && (this.includeLetters || this.includeNumbers || this.includeSymbols))
+  }
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value)
+
+    if (!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  generatePassword() {
+    const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+    const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const numbers = '0123456789'
+    const symbols = '!@#$%^&*()_-,.'
+
+    let availableCharacters = ''
+
+    if (this.includeLetters) {
+      availableCharacters += lowerCaseLetters
+      availableCharacters += upperCaseLetters
+    }
+
+    if (this.includeNumbers) {
+      availableCharacters += numbers
+    }
+
+    if (this.includeSymbols) {
+      availableCharacters += symbols
+    }
+
+    availableCharacters.split('')
+    const generatedPassword = []
+
+    for (let i = 0; i < this.passwordLength; i += 1) {
+      const max = availableCharacters.length
+      const ran = Math.random()
+      const idx = Math.floor(ran * (max + 1))
+
+      generatedPassword.push(availableCharacters[idx])
+
+    }
+
+    this.password = generatedPassword.join('')
+  }
 }
